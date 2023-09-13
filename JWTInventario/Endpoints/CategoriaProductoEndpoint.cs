af2edcc6660e730d51aeefec4bde387ee48bad19
Bryan Misael Sanchez Ramirez <misalbryan@gmail.com>
2023-09-12 21:57:38 -0600

    Creacion y configuracion de CategoriaProductoEndpoint

---
+++ JWTInventario/Endpoints/CategoriaProductoEndpoint.cs
@@ -0,0 +1,22 @@
+﻿namespace JWTInventario.Endpoints
+{
+    public static class CategoriaProductoEndpoint
+    {
+        static List<Object> data = new List<object>();
+
+        public static void AddProtectedEndpoints(this WebApplication app)
+        {
+            app.MapGet("/CategoriaProducto", () =>
+            {
+                return data;
+            });
+
+            app.MapPost("/CategoriaProducto", (string nameCategori) =>
+            {
+                data.Add(new { nameCategori });
+
+                return Results.Ok();
+            }).RequireAuthorization();
+        }
+    }
+}
