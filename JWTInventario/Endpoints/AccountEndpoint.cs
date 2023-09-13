a166fe3c779d06744941f10861947328eb30aa84
Bryan Misael Sanchez Ramirez <misalbryan@gmail.com>
2023-09-12 21:48:45 -0600

    Creacion y configuracion de AccountEndpoint

---
+++ JWTInventario/Endpoints/AccountEndpoint.cs
@@ -0,0 +1,24 @@
+﻿using JWTInventario.Auth;
+
+namespace JWTInventario.Endpoints
+{
+        public static class AccountEndpoint
+        {
+            public static void AddAccountEndoints(this WebApplication app)
+            {
+                app.MapPost("/account/login", (string login, string password, IJwtAuthenticationService authservice) =>
+                {
+                    if (login == "admin" && password == "12345")
+                    {
+                        var token = authservice.Authenticate(login);
+
+                        return Results.Ok(token);
+                    }
+                    else
+                    {
+                        return Results.Unauthorized();
+                    }
+                });
+            }
+        }
+}
